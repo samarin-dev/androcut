23c89de6680ccd83832d31f62fdaaf71e4d9cf2e
Mikhailo Samarin <59234993+samarin-dev@users.noreply.github.com>
2023-12-28 14:53:43 +0200

    Add files via upload

---
+++ vendor/develnext.bundle.gignight.LocalizationBundle/bundle/gignight/LocalizationException.php
@@ -0,0 +1,16 @@
+<?php
+namespace bundle\gignight\exception;
+
+/**
+ * Class LocalizationException
+ * 
+ * @author    GIGNIGHT
+ * @copyright 2019 - 2020 GIGNIGHT
+ * @license   MIT https://opensource.org/licenses/MIT
+ * @link      http://github.com/GIGNIGHT/dn-localization-ext
+ * @packages  localization
+ */
+class LocalizationException extends \Exception
+{
+
+}
