8056f7af931761ab74622669d48dccb517f720e4
Mikhailo Samarin <59234993+samarin-dev@users.noreply.github.com>
2023-12-28 14:55:20 +0200

    Add files via upload

---
+++ vendor/develnext.bundle.gignight.LocalizationBundle/bundle/gignight/exception/LocalizationFileNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+namespace bundle\gignight\exception;
+
+/**
+ * Class LocalizationFileNotFoundException
+ * 
+ * @author    GIGNIGHT
+ * @copyright 2019 - 2020 GIGNIGHT
+ * @license   MIT https://opensource.org/licenses/MIT
+ * @link      http://github.com/GIGNIGHT/dn-localization-ext
+ * @packages  localization
+ */
+abstract class LocalizationFileNotFoundException extends \Exception
+{
+
+}
